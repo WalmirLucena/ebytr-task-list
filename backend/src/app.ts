9d21c63d2cd759009db95fecbf38d35bb1b29c13
WalmirLucena <walmirlucena3@gmail.com>
2022-05-17 16:21:25 -0400

    feat: add App.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+class App {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.app.use(express.json());
+  }
+
+  public startServer(PORT: String | number = 30001): void {
+    this.app.listen(PORT, () => console.log(`Server running here http://localhost:${PORT}`));
+  }
+}
+
+export default App;
