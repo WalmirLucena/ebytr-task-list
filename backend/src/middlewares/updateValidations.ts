e410fc7cf2742964b65e394f9536825c099f7748
WalmirLucena <walmirlucena3@gmail.com>
2022-05-18 16:17:26 -0400

    feat: add updateValidations

---
+++ backend/src/middlewares/updateValidations.ts
@@ -0,0 +1,23 @@
+import { Response, NextFunction } from 'express';
+import Joi from 'joi';
+import { RequestWithBody } from '../controller/TaskController';
+import { update } from '../interfaces';
+
+const updateSchema = Joi.object().keys({
+  content: Joi.string().required(),
+  status: Joi.string().required().valid('A começar', 'Em desenvolvimento', 'Pendente', 'Finalizado'),
+});
+
+const updateValidations = async (
+  req: RequestWithBody<update>,
+  res: Response,
+  next: NextFunction,
+): Promise< typeof res | undefined> => {
+  const validation = updateSchema.validate(req.body);
+
+  if (validation.error) return res.status(400).json({ message: validation.error.message });
+
+  next();
+};
+
+export default updateValidations;
