cba5b871123e2c0440fdfa8c7c4693c6cf933a0d
WalmirLucena <walmirlucena3@gmail.com>
2022-05-19 09:34:24 -0400

    feat: adding test on TaskModel.create

---
+++ backend/src/tests/mocks/taskMocks.ts
@@ -0,0 +1,13 @@
+export const validTask = {
+    content: 'Testando aplicação',
+    publishedAt: "05/19/2022",
+    status: "Pendente"
+}
+
+export const validTaskArray = [
+    {
+        content: 'Testando aplicação',
+        publishedAt: "05/19/2022",
+        status: "Pendente"
+    }
+]
--- backend/src/tests/unit/models/taskModel.test.ts
+++ backend/src/tests/unit/models/taskModel.test.ts
@@ -1,12 +1,23 @@
 import { expect } from "chai";
 import * as Sinon from "sinon";
+import { Task as ITask } from "../../../interfaces";
 import TaskModel from "../../../models/TaskModel";
+import { validTask } from "../../mocks/taskMocks";
 
 const Task = new TaskModel();
 
 describe("Task Model Test", () => {
     describe("Create a Task with correct data", ()=> {
         before(()=> 
-        Sinon.stub(Task, "create").resolves(TaskMock));
+        Sinon.stub(Task, "create").resolves(validTask as ITask));
+    })
+
+    after(()=> {
+        Sinon.restore();
+    })
+
+    it("Return a new Task", async () => {
+        const task = await Task.create(validTask);
+        expect(task).to.be.deep.equal(validTask);
     })
 })
