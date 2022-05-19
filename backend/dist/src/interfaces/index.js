7f7549dd3fb73a6e515aa8d28ff2a6b10c6dbc83
WalmirLucena <walmirlucena3@gmail.com>
2022-05-19 07:55:08 -0400

    fix: .gitignore

--- backend/dist/src/interfaces/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TaskSchema = void 0;
-const TaskInterface_1 = require("./TaskInterface");
-Object.defineProperty(exports, "TaskSchema", { enumerable: true, get: function () { return TaskInterface_1.TaskSchema; } });
