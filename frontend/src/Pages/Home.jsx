ae2811287f7f24b3ba5e0acb3b871b24633b20c6
WalmirLucena <walmirlucena3@gmail.com>
2022-05-20 09:16:46 -0400

    feat: function to create Task and getTask and render

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Input from '../Components/Inputs';
 import { request } from '../Services/request';
 
 function Home() {
+  const [task, setTask] = useState([]);
+  const [newTask, setNewTask] = useState(false);
+
   const createTask = async (data) => {
     const endpoint = '/task';
     const body = { ...data, publishedAt: new Date() };
-    console.log(body);
-    const response = await request(endpoint, body, 'post');
-    console.log(response);
+    await request(endpoint, body, 'post');
+    setNewTask(!newTask);
+  };
+
+  const getTask = async () => {
+    const endpoint = '/task';
+    const response = await request(endpoint, {}, 'get');
+    setTask(response);
   };
 
   const getValues = (data) => {
     createTask(data);
   };
+
+  useEffect(() => {
+    getTask();
+  }, [newTask]);
 
   return (
     <>
@@ -21,6 +33,30 @@ function Home() {
         Ebytr To-do List
       </h1>
       <Input getValues={getValues} />
+      <div className="flex justify-center mb-3 relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <th>Tarefa</th>
+            <th>Data</th>
+            <th>Status</th>
+            <th>Deletar</th>
+            <th>Editar</th>
+          </thead>
+          <tbody>
+            {task.map(({ content, publishedAt, status }) => (
+              <tr key={publishedAt}>
+                <td>{content}</td>
+                <td>{publishedAt}</td>
+                <td>{status}</td>
+                <th>Deletar</th>
+                <th>Editar</th>
+              </tr>
+            ))}
+          </tbody>
+
+        </table>
+      </div>
+
     </>
   );
 }
